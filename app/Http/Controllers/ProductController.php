72d8aed7529b2d99ab98340010fa66ecd630b290
singhWaraich <singh4narender@gmail.com>
2020-10-24 12:03:22 +0530

    image path update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -174,31 +174,31 @@ class ProductController extends Controller
         if($request->image){
             $file = $request->image;
             $filename = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path('public/images/products'), $filename);
+            $file->move(public_path('images/products'), $filename);
             $data['image'] = $filename;
         }
         if($request->image1){
             $file = $request->image1;
             $filename1 = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path('public/images/products'), $filename1);
+            $file->move(public_path('images/products'), $filename1);
             $data['image1'] = $filename1;
         }
         if($request->image2){
             $file = $request->image2;
             $filename2 = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path('public/images/products'), $filename2);
+            $file->move(public_path('images/products'), $filename2);
             $data['image2'] = $filename2;
         }
         if($request->image3){
             $file = $request->image3;
             $filename3 = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path('public/images/products'), $filename3);
+            $file->move(public_path('images/products'), $filename3);
             $data['image3'] = $filename3;
         }
         if($request->image4){
             $file = $request->image4;
             $filename4 = time().'.'.$file->getClientOriginalExtension();
-            $file->move(public_path('public/images/products'), $filename4);
+            $file->move(public_path('images/products'), $filename4);
             $data['image4'] = $filename4;
         }
         //dd($data);
